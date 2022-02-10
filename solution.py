72d8fdea7a90212419ef9f46ea0513e364c5efc4
Lee Segal <lsegal@blend.com>
2022-02-10 07:22:11 -0800

    updated hw

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 # import socket module
+from cgi import test
 from multiprocessing import connection
 from socket import *
 # In order to terminate the program
@@ -12,21 +13,21 @@ def webServer(port=13331):
   serverSocket.listen(1)
   while True:
     #Establish the connection
-    print('Ready to serve...')
+    # print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
+    # print("test")
     try:
       try:
         print("in first try")
         message = connectionSocket.recv(1024).decode(set)
         filename = message.split()[1]
         f = open(filename[1:])
-        print(f)
-        outputdata = f
+        # print(f)
+        outputdata = f.read()
+        # print(outputdata)
         
         #Send one HTTP header line into socket.
-        #Fill in start
-
-        #Fill in end
+        connectionSocket.send("200 OK".encode())
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
@@ -36,14 +37,14 @@ def webServer(port=13331):
         connectionSocket.close()
       except IOError:
         # Send response message for file not found (404)
-        message_not_found = "file not found"
+        message_not_found = "404 not found"
         connectionSocket.send((message_not_found.upper()).encode())
         
         #Close client socket
         connectionSocket.close()
 
     except (ConnectionResetError, BrokenPipeError):
-      connectionSocket.close()
+      pass
 
   serverSocket.close()
   sys.exit()  # Terminate the program after sending the corresponding data
