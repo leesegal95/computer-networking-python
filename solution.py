2671e77058498e2596c13b0d1e3c88442b76d9ca
Lee Segal <lsegal@blend.com>
2022-02-23 10:43:53 -0800

    added updates to code

--- solution.py
+++ solution.py
@@ -8,9 +8,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-    mailserver_test = (mailserver,port)
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect(mailserver_test)
+    clientSocket.connect((mailserver,port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
@@ -30,8 +29,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
         
 
     # Send MAIL FROM command and print server response.
-    mailfrom = 'MAIL FROM: <test@client.net>\r\n'
-    clientSocket.send(mailfrom.encode())
+    MAILFROM = 'MAIL FROM: <test@client.net>\r\n'
+    clientSocket.send(MAILFROM.encode())
     recv1 = clientSocket.recv(1024).decode()
     # print(recv1)
     if recv1[:3] != '250':
@@ -39,8 +38,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
         pass
 
     # Send RCPT TO command and print server response.
-    RCPTto = 'RCPT TO: <test@recipient.net>\r\n'
-    clientSocket.send(RCPTto.encode())
+    RCPT = 'RCPT TO: <test@recipient.net>\r\n'
+    clientSocket.send(RCPT.encode())
     recv1 = clientSocket.recv(1024).decode()
     # print(recv1)
     if recv1[:3] != '250':
@@ -48,8 +47,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
        pass
 
     # Send DATA command and print server response.
-    data = 'Message data\r\n'
-    clientSocket.send(data.encode())
+    DATA = 'Message data\r\n'
+    clientSocket.send(DATA.encode())
     recv1 = clientSocket.recv(1024).decode()
     # print(recv1)
     if recv1[:3] != '250':
@@ -57,8 +56,9 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
         pass
 
     # Send message data.
-    msg_data = 'Date: Wed, 30 July 2019 06:04:34, From: test@client.net, Subject: How SMTP works, To: user@recipient.net, Body text\r\n'
-    clientSocket.send(msg_data.encode())
+    # msg_data = 'Date: Wed, 30 July 2019 06:04:34, From: test@client.net, Subject: How SMTP works, To: user@recipient.net, Body text\r\n'
+    # msg_data = msg
+    clientSocket.send(msg.encode())
     recv1 = clientSocket.recv(1024).decode()
     # print(recv1)
     if recv1[:3] != '250':
@@ -66,8 +66,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
        pass
 
     # Message ends with a single period.
-    period = '\r\n.\r\n'
-    clientSocket.send(period.encode())
+    # period = '\r\n.\r\n'
+    clientSocket.send(endmsg.encode())
     recv1 = clientSocket.recv(1024).decode()
     # print(recv1)
     if recv1[:3] != '250':
